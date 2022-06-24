635c877f6bfe57f0f08750f6f92ebf496f578596
onifade <onifadejoshua15@gmial.com>
2022-06-24 05:56:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+//Student Details
+let studentName = "Onifade Joshua";
+let studentHeight = 6.0;
+let studentCountry = "Nigeria";
+//Using document.write() function 
+document.write("<h1>Student Details</h1>");
+document.write("<h3>Onifade Joshua</h3>"); //Student Name;
+document.write("<h3>6.0 feets tall</h3>"); //Student Height;
+document.write("<h3>Nigeria</h3>"); //Student Country;
+// document.write(`${studentName} ${studentHeight} ${studentCountry}`);
+
+//using alert() function
+alert(`Student name is ${studentName}.`); //Student Name;
+alert(`Student height is ${studentHeight} feets.`); //Student Height;
+alert(`Student country is ${studentCountry}.`); //Student Country;
